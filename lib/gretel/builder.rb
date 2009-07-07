93015874263dfe8e5cb23a164b4481b2b812ee30
Nolan Eakins <sneakin@semanticgap.com>
2009-07-07 11:36:10 -0400

    Added a few additional options. Changed the output to an unordered list.

--- lib/gretel/builder.rb
+++ lib/gretel/builder.rb
@@ -19,30 +19,54 @@ module Gretel
       @request_path = request_path
       @separator = options[:separator] || '&raquo;'
       @dom_id = options[:dom_id] || 'breadcrumb_trail'
+      @skip_index = options[:skip_index]
+      @skip_home = !options[:home]
+      @reverse = options[:reverse]
+      @prefix = options[:prefix]
+      @prefix_separator = options[:prefix_separator] || ': '
+      @skip_resource = options[:skip_resource]
+      @max_depth = options[:max_depth]
     end
 
     def to_s
       levels = request_path.split('?')[0].split('/')
       levels.delete_at(0)
+      levels.pop if @skip_resource
 
-      links = "<a href='/'>home</a>"
+      links = Array.new
+      links << "<a href='/'>home</a>" unless @skip_home
 
       levels.each_with_index do |level, index|
-        text, href = extract_text_and_href(level, levels[0..index])
-        links += %( #{separator} <a href="#{href}">#{text.to_s.downcase}</a>)
+        href = File.join("/", levels[0..index].join('/'))
+        text = extract_model_from_path(href)
+        next if @skip_index && text.nil? # && level != levels.last
+
+        text ||= level.downcase.gsub('_', ' ').titleize
+
+        if href == request_path
+          links << text.to_s
+        else
+          links << %(<a href="#{href}">#{text.to_s}</a>)
+        end
       end
+
+      links.reverse! if @reverse
+      links = links.collect_with_index do |link, index|
+        link = "#{@prefix}#{@prefix_separator}#{link}" if index == 0 && @prefix
+        link = "<li class=\"item-#{index}\">#{link}</li>"
+      end
+      # links.unshift("<li class=\"item-prefix\">#{@prefix}</li>") if @prefix
+      links = links[0, @max_depth] if @max_depth
 
       %(
       <div id="#{dom_id}">
-      <p>#{links}</p>
+      <ul>#{links.join}</ul>
       </div>
       )
     end
 
     protected
     def extract_text_and_href(text, href_elements)
-      path = File.join("/", href_elements.join('/'))
-      return [extract_model_from_path(path) || text.downcase.gsub('_', ' '), path]
     end
 
     def model_extractor
